ea41d75f066c4caf62c48d5ff429ed2e31714329
Tomas Michlig <70540989+michtomas@users.noreply.github.com>
2022-02-03 11:49:32 -0300

    Update sin título.pas

--- practica0/sin título.pas
+++ practica0/sin título.pas
@@ -1,3 +1,11 @@
+{Realice un programa que informe el valor total en pesos de una transacción en dólares.
+Para ello, el programa debe leer el monto total en dólares de la transacción, el valor del
+1 dólar al día de la fecha y el porcentaje (en pesos) de la comisión que cobra el banco por la
+transacción. Por ejemplo, si la transacción se realiza por 10 dólares, el dólar tiene un valor
+20,54 pesos y el banco cobra un 4% de comisión, entonces el programa deberá informar:
+La transacción será de 213,61 pesos argentinos
+(resultado de multiplicar 10*20,54 y adicionarle el 4%)}
+
 program ej6;
 	type codigos=1..5;
 var
